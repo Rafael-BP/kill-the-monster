2b956f00e669db6b6f6733e84d2062a979ab8a48
Rafael Bartalotti Pinto <rafael_bartalotti@hotmail.com>
2014-10-29 02:25:17 -0200

    Mensagem de vitória, botoes de ataque somem na vitória e botao de voltar

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 require 'sistema.php';
 inicio();
 ?>
@@ -8,19 +8,29 @@ inicio();
         <title>Mate o monstro!</title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style type="text/css">
+            .oculto{
+                visibility: hidden;
+                display: none;
+            }
+        </style>    
     </head>
     <body>
+        <div id="vitoria" class="oculto" style="color: green;">Parabens! você derrotou este terrivel monstro!</div>
         <h1 id="nome"><?php obterMonstro()['nome'] ?></h1>
         <div id="imagem"><?php obterMonstro()['imagem'] ?></div><!-- Aqui colocar tag para ir imagem do monstro -->
         <div id="vida"><?php obterMonstro()['vida'] ?></div><!-- Aqui colocar uma progress bar com a % de vida -->
         <div id="dano" style="color: red;"></div>
         <div id="danoTotal" style="color: red;"></div>
-        <br/>
-        <button onclick="causarDano()">Atacar fisicamente!</button>
-        <br/>
-        <button onclick="causarDano()">Atacar a distancia!</button>
-        <br/>
-        <button onclick="causarDano()">Usar magia!</button>
+        <div id="botoesAtaque">
+            <br/>      
+            <button onclick="causarDano()">Atacar fisicamente!</button>
+            <br/>
+            <button onclick="causarDano()">Atacar a distancia!</button>
+            <br/>
+            <button onclick="causarDano()">Usar magia!</button> <br/>
+        </div>
+        <button id="voltar">Voltar</button><!-- Aqui deve voltar para o menu inicial -->
     </body>
 </html>
 
@@ -29,6 +39,10 @@ inicio();
     atualizarDados();
     danoTotal = 0;
     function atualizarDados() {
+        if(parseInt($('#vida').text()) <= 0) {
+            $('#botoesAtaque').addClass('oculto');
+            $('#vitoria').removeClass('oculto');
+        }
         $.ajax({
             type: "GET",
             url: "atualiza.php?monstro=1",
@@ -39,7 +53,7 @@ inicio();
                 $('#vida').text(res[2]);                
             }
         });
-        setTimeout(atualizarDados, 500);
+        setTimeout(atualizarDados, 100);
     }
     function causarDano() {
         $.ajax({
