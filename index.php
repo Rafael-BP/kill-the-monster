8162d5494bca1aa3ffd32cdad8ee37f8365d5c55
gabrielkamimura <gabrielk.yano@gmail.com>
2014-11-06 00:12:16 -0200

    Carregamento de JQuery #1

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                 </table>
             </div>
         </div>
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="assets/js/jquery.min.js" type="text/javascript"></script>
         <script type="text/javascript" src="assets/js/bootstrap.min.js"></script> 
 
     </body>
